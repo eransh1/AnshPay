9ba0c04f4a16c09c7f149b79590ea1ef14c38764
ansh <eranshbansal@gmail.com>
2022-08-22 23:18:39 +0530

    updated error path in app.js

--- src/App.js
+++ src/App.js
@@ -13,7 +13,7 @@ function App(){
     <Route path="/AnshPay" element={<Home />}/>
     <Route path="/about" element={<About />}/>
     <Route path="/contact" element={<Contact />}/>
-    <Route element={<Error />}/>
+    <Route path="*" element={<Error />}/>
     <Route path="/services" element={<Services />}/>
    </Routes>
     </>
