236503bc5121e16a78437e22628a2aefd6021edb
ansh <eranshbansal@gmail.com>
2022-08-22 21:07:13 +0530

    updated app.js homepage url

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ function App(){
   return (
     <>
     <Routes >
-    <Route path="/" element={<Home />}/>
+    <Route path="/AnshPay" element={<Home />}/>
     <Route path="/about" element={<About />}/>
     <Route path="/contact" element={<Contact />}/>
     <Route path="*" element={<Error />}/>
